c0976ea8bf23c28d42a37590a188815fe33f40d8
mikesaraus <mizkie98@gmail.com>
2022-10-15 08:56:04 +0800

    Debugging enabled, Video eager loading and background sybnc on sw

--- js/unsupported-spa-handler.js
+++ js/unsupported-spa-handler.js
@@ -1,7 +1,51 @@
 /**
- * Growmodo Hub v0.4.14-dev
- * (c) 2022 Growmodo, GmbH
+ * Author: Mike Saraus
+ * https://github.com/mikesaraus
  *
- * @preserve
+ * If SPA is Unsupported (e.g.: Github Pages)
+ *
+ * @preserve (c) 2022
  */
-(function(e,n){function a(){if("/"===e.search[1]){const n=e.search.slice(1).split("&").map((function(e){return e.replace(/~and~/g,"&")})).join("?");window.history.replaceState(null,null,e.pathname.slice(0,-1)+n+e.hash)}}function t(){e.href=e.protocol+"//"+e.hostname+(e.port?":"+e.port:"")+e.pathname.split("/").slice(0,1+n).join("/")+"/?/"+e.pathname.slice(1).split("/").slice(n).join("/").replace(/&/g,"~and~")+(e.search?"&"+e.search.slice(1).replace(/&/g,"~and~"):"")+e.hash}"404"===document.title?t():a()})(window.location,0);
+(function (location, pathSegments) {
+  function resolve() {
+    if (location.search[1] === '/') {
+      const decoded = location.search
+        .slice(1)
+        .split('&')
+        .map(function (s) {
+          return s.replace(/~and~/g, '&');
+        })
+        .join('?');
+      window.history.replaceState(
+        null,
+        null,
+        location.pathname.slice(0, -1) + decoded + location.hash
+      );
+    }
+  }
+
+  function redirect() {
+    location.href =
+      location.protocol +
+      '//' +
+      location.hostname +
+      (location.port ? ':' + location.port : '') +
+      location.pathname
+        .split('/')
+        .slice(0, 1 + pathSegments)
+        .join('/') +
+      '/?/' +
+      location.pathname
+        .slice(1)
+        .split('/')
+        .slice(pathSegments)
+        .join('/')
+        .replace(/&/g, '~and~') +
+      (location.search
+        ? '&' + location.search.slice(1).replace(/&/g, '~and~')
+        : '') +
+      location.hash;
+  }
+
+  document.title === '404' ? redirect() : resolve();
+})(window.location, 0);
