7c696f2fae814267bead79b8bbc1144c21f12c01
mikesaraus <mizkie98@gmail.com>
2022-11-10 14:03:39 +0800

    Fixes

--- js/unsupported-spa-handler.js
+++ js/unsupported-spa-handler.js
@@ -1,7 +1,7 @@
 /**
- * Growmodo Hub v0.5.9-dev
- * (c) 2022 Growmodo, GmbH
- *
- * @preserve
- */
+* Growmodo Hub v0.5.9-dev
+* (c) 2022 Growmodo, GmbH
+*
+* @preserve
+*/
 (function(e,n){function a(){if("/"===e.search[1]){const n=e.search.slice(1).split("&").map((function(e){return e.replace(/~and~/g,"&")})).join("?");window.history.replaceState(null,null,e.pathname.slice(0,-1)+n+e.hash)}}function t(){e.href=e.protocol+"//"+e.hostname+(e.port?":"+e.port:"")+e.pathname.split("/").slice(0,1+n).join("/")+"/?/"+e.pathname.slice(1).split("/").slice(n).join("/").replace(/&/g,"~and~")+(e.search?"&"+e.search.slice(1).replace(/&/g,"~and~"):"")+e.hash}"404"===document.title?t():a()})(window.location,0);
