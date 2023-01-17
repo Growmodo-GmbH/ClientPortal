fd7a200ba90179f28a47ebb4efd5c39c5e0b5e6e
Mike <16617782+mikesaraus@users.noreply.github.com>
2023-01-17 20:52:14 +0800

    Fixes v0.7.13

---
+++ js/913.288560ca.js
@@ -0,0 +1,8 @@
+/**
+* Growmodo Hub v0.7.13
+* (c) 2023 Growmodo, GmbH
+* https://hub.growmodo.dev
+*
+* @preserve
+*/
+'use strict';var a29J=a29b;(function(a,b){var I=a29b,c=a();while(!![]){try{var d=parseInt(I(0x9f))/0x1+-parseInt(I(0x99))/0x2*(parseInt(I(0xa1))/0x3)+-parseInt(I(0xa0))/0x4+-parseInt(I(0x97))/0x5*(parseInt(I(0xa7))/0x6)+-parseInt(I(0x96))/0x7+parseInt(I(0x9b))/0x8*(-parseInt(I(0xa2))/0x9)+-parseInt(I(0xa4))/0xa*(-parseInt(I(0x9d))/0xb);if(d===b)break;else c['push'](c['shift']());}catch(e){c['push'](c['shift']());}}}(a29a,0xe8af0));function a29b(a,b){var c=a29a();return a29b=function(d,e){d=d-0x96;var f=c[d];return f;},a29b(a,b);}function a29a(){var M=['1332815ikqCgm','7121544nhWUud','741XQkXeR','396zkcJBT','required','40366770kRyQSV','textarea','webpackChunkgrowmodo_hub','263178ZWITOu','Write\x20something...','components','1476125AUirfD','80pxYmmZ','rules','11098ZpZvoe','render','64136TPwPbh','q-input','11zuGtDS','push'];a29a=function(){return M;};return a29a();}(globalThis[a29J(0xa6)]=globalThis[a29J(0xa6)]||[])[a29J(0x9e)]([[0x391],{0x13829:(b,f,g)=>{var L=a29J;g['r'](f),g['d'](f,{'default':()=>z});var h=g(0xe9bb);function j(A,B,C,D,E,F){var K=a29b;const G=(0x0,h['up'])(K(0x9c));return(0x0,h['wg'])(),(0x0,h['j4'])(G,{'outlined':'','autofocus':'','rows':'15','type':K(0xa5),'placeholder':K(0xa8),'class':'rounded\x20text-body2\x20input-white','rules':[H=>!A[K(0xa3)]||(!!H||'')]},null,0x8,[K(0x98)]);}var k=g(0xc866);const m=(0x0,h['aZ'])({'name':'TextArea1','mixins':[k['kE']],'props':{'required':{'type':Boolean,'default':!0x1}}});var q=g(0x2d77),v=g(0x10433),w=g(0x11160),x=g['n'](w);const y=(0x0,q['Z'])(m,[[L(0x9a),j]]),z=y;x()(m,L(0xa9),{'QInput':v['Z']});}}]);
--- js/unsupported-spa-handler.js
+++ js/unsupported-spa-handler.js
@@ -1,5 +1,5 @@
 /**
-* Growmodo Hub v0.7.12
+* Growmodo Hub v0.7.13
 * (c) 2023 Growmodo, GmbH
 * https://hub.growmodo.dev
 *
