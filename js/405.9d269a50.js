b4ac009cea2d73e48ab7eb8a7a1c3c2a41495e71
Mike Saraus <mizkie98@gmail.com>
2023-05-26 12:34:04 +0800

    Updates and fixes

---
+++ js/405.9d269a50.js
@@ -0,0 +1,76 @@
+"use strict";
+(globalThis["webpackChunkgrowmodo_hub"] = globalThis["webpackChunkgrowmodo_hub"] || []).push([[405],{
+
+/***/ 54405:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ EmptyLayout)
+});
+
+// EXTERNAL MODULE: ./node_modules/vue/dist/vue.esm-bundler.js + 6 modules
+var vue_esm_bundler = __webpack_require__(56646);
+;// CONCATENATED MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/loader.js.transform-quasar-imports.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-2.use[0]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@quasar/app-webpack/lib/webpack/loader.vue.auto-import-quasar.js??ruleSet[0].use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./src/layouts/EmptyLayout.vue?vue&type=template&id=39546293
+
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_router_view = (0,vue_esm_bundler/* resolveComponent */.up)("router-view");
+  const _component_q_page_container = (0,vue_esm_bundler/* resolveComponent */.up)("q-page-container");
+  const _component_q_layout = (0,vue_esm_bundler/* resolveComponent */.up)("q-layout");
+  return (0,vue_esm_bundler/* openBlock */.wg)(), (0,vue_esm_bundler/* createBlock */.j4)(_component_q_layout, {
+    view: "LHh lpR lFf"
+  }, {
+    default: (0,vue_esm_bundler/* withCtx */.w5)(() => [(0,vue_esm_bundler/* createVNode */.Wm)(_component_q_page_container, {
+      class: "bg-color"
+    }, {
+      default: (0,vue_esm_bundler/* withCtx */.w5)(() => [(0,vue_esm_bundler/* createVNode */.Wm)(_component_router_view, null, {
+        default: (0,vue_esm_bundler/* withCtx */.w5)(({
+          Component
+        }) => [((0,vue_esm_bundler/* openBlock */.wg)(), (0,vue_esm_bundler/* createBlock */.j4)((0,vue_esm_bundler/* resolveDynamicComponent */.LL)(Component)))]),
+        _: 1
+      })]),
+      _: 1
+    })]),
+    _: 1
+  });
+}
+;// CONCATENATED MODULE: ./src/layouts/EmptyLayout.vue?vue&type=template&id=39546293
+
+;// CONCATENATED MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/loader.js.transform-quasar-imports.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-2.use[0]!./node_modules/@quasar/app-webpack/lib/webpack/loader.vue.auto-import-quasar.js??ruleSet[0].use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./src/layouts/EmptyLayout.vue?vue&type=script&lang=js
+
+/* harmony default export */ const EmptyLayoutvue_type_script_lang_js = ((0,vue_esm_bundler/* defineComponent */.aZ)({
+  name: 'EmptyLayout'
+}));
+;// CONCATENATED MODULE: ./src/layouts/EmptyLayout.vue?vue&type=script&lang=js
+ 
+// EXTERNAL MODULE: ./node_modules/vue-loader/dist/exportHelper.js
+var exportHelper = __webpack_require__(11639);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/layout/QLayout.js
+var QLayout = __webpack_require__(20249);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/page/QPageContainer.js
+var QPageContainer = __webpack_require__(12133);
+// EXTERNAL MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/runtime.auto-import.js
+var runtime_auto_import = __webpack_require__(69984);
+var runtime_auto_import_default = /*#__PURE__*/__webpack_require__.n(runtime_auto_import);
+;// CONCATENATED MODULE: ./src/layouts/EmptyLayout.vue
+
+
+
+
+;
+const __exports__ = /*#__PURE__*/(0,exportHelper/* default */.Z)(EmptyLayoutvue_type_script_lang_js, [['render',render]])
+
+/* harmony default export */ const EmptyLayout = (__exports__);
+;
+
+
+runtime_auto_import_default()(EmptyLayoutvue_type_script_lang_js, 'components', {QLayout: QLayout/* default */.Z,QPageContainer: QPageContainer/* default */.Z});
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=405.9d269a50.js.map
---
+++ js/713.a13d54af.js
@@ -0,0 +1,111 @@
+"use strict";
+(globalThis["webpackChunkgrowmodo_hub"] = globalThis["webpackChunkgrowmodo_hub"] || []).push([[713],{
+
+/***/ 10713:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ RequestsPage)
+});
+
+// EXTERNAL MODULE: ./node_modules/vue/dist/vue.esm-bundler.js + 6 modules
+var vue_esm_bundler = __webpack_require__(56646);
+;// CONCATENATED MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/loader.js.transform-quasar-imports.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-2.use[0]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@quasar/app-webpack/lib/webpack/loader.vue.auto-import-quasar.js??ruleSet[0].use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./src/pages/RequestsPage.vue?vue&type=template&id=32f0336c
+
+const _hoisted_1 = {
+  class: "font-medium text-h5 text-primary-700"
+};
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_q_toolbar_title = (0,vue_esm_bundler/* resolveComponent */.up)("q-toolbar-title");
+  const _component_q_toolbar = (0,vue_esm_bundler/* resolveComponent */.up)("q-toolbar");
+  const _component_q_card = (0,vue_esm_bundler/* resolveComponent */.up)("q-card");
+  const _component_TaskProjectList = (0,vue_esm_bundler/* resolveComponent */.up)("TaskProjectList");
+  const _component_q_page = (0,vue_esm_bundler/* resolveComponent */.up)("q-page");
+  return (0,vue_esm_bundler/* openBlock */.wg)(), (0,vue_esm_bundler/* createBlock */.j4)(_component_q_page, {
+    padding: "",
+    class: "container"
+  }, {
+    default: (0,vue_esm_bundler/* withCtx */.w5)(() => [(0,vue_esm_bundler/* createVNode */.Wm)(_component_q_card, {
+      flat: "",
+      bordered: "",
+      class: "rounded card-padding-title"
+    }, {
+      default: (0,vue_esm_bundler/* withCtx */.w5)(() => [(0,vue_esm_bundler/* createVNode */.Wm)(_component_q_toolbar, {
+        class: "q-pa-none no-min-height"
+      }, {
+        default: (0,vue_esm_bundler/* withCtx */.w5)(() => [(0,vue_esm_bundler/* createVNode */.Wm)(_component_q_toolbar_title, null, {
+          default: (0,vue_esm_bundler/* withCtx */.w5)(() => [(0,vue_esm_bundler/* createElementVNode */._)("div", _hoisted_1, (0,vue_esm_bundler/* toDisplayString */.zw)(_ctx.title), 1)]),
+          _: 1
+        })]),
+        _: 1
+      })]),
+      _: 1
+    }), (0,vue_esm_bundler/* createVNode */.Wm)(_component_TaskProjectList, {
+      class: "q-my-lg"
+    })]),
+    _: 1
+  });
+}
+;// CONCATENATED MODULE: ./src/pages/RequestsPage.vue?vue&type=template&id=32f0336c
+
+// EXTERNAL MODULE: ./src/components/Requests/TaskProjectList.vue + 4 modules
+var TaskProjectList = __webpack_require__(12940);
+;// CONCATENATED MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/loader.js.transform-quasar-imports.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-2.use[0]!./node_modules/@quasar/app-webpack/lib/webpack/loader.vue.auto-import-quasar.js??ruleSet[0].use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./src/pages/RequestsPage.vue?vue&type=script&lang=js
+
+
+/* harmony default export */ const RequestsPagevue_type_script_lang_js = ((0,vue_esm_bundler/* defineComponent */.aZ)({
+  name: 'RequestsPage',
+  components: {
+    TaskProjectList: TaskProjectList/* default */.Z
+  },
+  data() {
+    return {
+      title: 'Requests'
+    };
+  },
+  async mounted() {
+    this.$root.setMeta({
+      title: this.title
+    });
+  }
+}));
+;// CONCATENATED MODULE: ./src/pages/RequestsPage.vue?vue&type=script&lang=js
+ 
+// EXTERNAL MODULE: ./node_modules/vue-loader/dist/exportHelper.js
+var exportHelper = __webpack_require__(11639);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/page/QPage.js
+var QPage = __webpack_require__(69885);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/card/QCard.js
+var QCard = __webpack_require__(44458);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/toolbar/QToolbar.js
+var QToolbar = __webpack_require__(51663);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/toolbar/QToolbarTitle.js
+var QToolbarTitle = __webpack_require__(81973);
+// EXTERNAL MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/runtime.auto-import.js
+var runtime_auto_import = __webpack_require__(69984);
+var runtime_auto_import_default = /*#__PURE__*/__webpack_require__.n(runtime_auto_import);
+;// CONCATENATED MODULE: ./src/pages/RequestsPage.vue
+
+
+
+
+;
+const __exports__ = /*#__PURE__*/(0,exportHelper/* default */.Z)(RequestsPagevue_type_script_lang_js, [['render',render]])
+
+/* harmony default export */ const RequestsPage = (__exports__);
+;
+
+
+
+
+runtime_auto_import_default()(RequestsPagevue_type_script_lang_js, 'components', {QPage: QPage/* default */.Z,QCard: QCard/* default */.Z,QToolbar: QToolbar/* default */.Z,QToolbarTitle: QToolbarTitle/* default */.Z});
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=713.a13d54af.js.map
